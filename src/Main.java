76847d30a837b1448035a4b6b1ea72fb0c75fb5c
1E10 Laptop <sample@bths.edu>
2022-10-07 10:44:48 -0400

    2

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1988;
+
+        int b = 1990;
+
+
+
+        String claim = " that the world’s athletes " +
+
+                "competed in Olympic Games in ";
+
+
+
+        String s = "It is " + true + claim + a +
+
+                " but " + false + claim + b + ".";
+
+
+
+        System.out.println(s);
+
+    }
+}
